9e0ed28df2a6e465069d4b73d645ad94d8e10ac4
Shahar Har-Shuv <shachar.harshuv@gmail.com>
2025-10-10 22:09:02 -0400

    Fix table order (#13)
    * Fix table order
    
    * remove unnecessary line

--- src/israeliwhist.ts
+++ src/israeliwhist.ts
@@ -297,12 +297,13 @@ class IsraeliWhist extends GameGui<IsraeliWhistGamedatas> {
       html` <div id="player-tables"></div> `,
     );
 
-    const players = [...Object.values(this.gamedatas.players)];
-    while (+players[0].id != this.player_id) {
-      players.push(players.shift());
+    const playerorder = [...this.gamedatas.playerorder];
+    while (playerorder[0] != this.player_id) {
+      playerorder.push(playerorder.shift());
     }
 
-    players.forEach((player, index) => {
+    playerorder.forEach((playerId, index) => {
+      const player = this.gamedatas.players[playerId];
       document.getElementById("player-tables").insertAdjacentHTML(
         "beforeend",
         html`
